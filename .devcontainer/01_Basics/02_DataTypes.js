bda70a610d88600223e3a15af7b0b9b68f062ad6
asif-nawaz01 <asifnawaz51111@gmail.com>
2026-01-08 21:59:31 +0530

    Conversion Operation

---
+++ .devcontainer/01_Basics/02_DataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // Treat all JS codes as newer version
+
+//alert (3 + 3) // we are using node.js, not browser
+
+let name = "Nawaz"
+let age = 22
+let isLoggedIn = false
+let state;
+
+//number => 2 to power 53
+//bigint => larger than 2 to power 53
+//string => "" , '' , ``
+//boolean => true/false
+//null => standalone value
+//undefined => standalone value
+//symbol => unique identifiers
+
+//object => key : value pairs
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isLoggedIn);
+console.log(typeof state);
