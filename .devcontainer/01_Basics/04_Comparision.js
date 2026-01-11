007998f78bc8a4ab88f91b5bd5e813b04d9d5c3d
asif-nawaz01 <asifnawaz51111@gmail.com>
2026-01-11 11:21:46 +0530

    Comparision Update

---
+++ .devcontainer/01_Basics/04_Comparision.js
@@ -0,0 +1,26 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+/*`` Comparison with type conversion ``*/
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 1);
+// console.log(null >= 0);
+// console.log(null == 0);
+
+// console.log(undefined > 1);
+// console.log(undefined < 1);
+// console.log(undefined == 1);
+// console.log(undefined == undefined);
+//nsole.log(NaN == NaN);
+
+// === strict equality operator
+console.log(2 === 2);
+console.log("2" === 2);
+console.log(false === 0);
