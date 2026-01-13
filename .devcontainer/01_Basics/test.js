ac3082d68d61738001c22381413f853353767bcd
asif-nawaz01 <asifnawaz51111@gmail.com>
2026-01-13 15:24:44 +0530

    INDEX

--- .devcontainer/01_Basics/test.js
+++ .devcontainer/01_Basics/test.js
@@ -1,1 +1,17 @@
-console.log("Nawaz")
+console.log("Nawaz");
+console.log("As we can start the devcontainer now");
+console.log("This is test file");
+console.log("This file is to test the changes made in devcontainer");
+
+console.log("This file contains the various topics like:-")
+console.log("1. Basics of JavaScript");
+console.log("2. Variables");
+console.log("3. Data Types");
+console.log("4. Conversion and Operations");
+console.log("5. Comparisions");
+console.log("6. Strings");
+console.log("7. Numbers and Maths");
+console.log("8. Date and Time");
+console.log("9. Data-Types Summary");
+
+console.log("TO BE CONTINUED...");
